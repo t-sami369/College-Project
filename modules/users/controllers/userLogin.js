d5930b50cd55e9415d6abef8c2be05da6ab006fd
Samiksha Tiwari <incredibleitsme404@gmail.com>
2024-11-27 14:03:57 +0545

    fixed error

--- modules/users/controllers/userLogin.js
+++ modules/users/controllers/userLogin.js
@@ -1,9 +1,10 @@
 const mongoose=require("mongoose");
+const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken");
 
 const userLogin=async(req,res)=>{
     const Users=mongoose.model("users");
-    const [email,password] = req.body;
+    const {email,password} = req.body;
 
      try{
        if(!email) throw "Email must be provided.";
@@ -33,7 +34,8 @@ const userLogin=async(req,res)=>{
       expiresIn:"90 days"
      });
     res.status(200).json({
-     status:"User successfully logged in."
+     status:"User successfully logged in.",
+     accessToken
     })
  }
  module.exports=userLogin;
