c7f945707f58dbc57ff97dd9c497525afb05e09b
Samiksha Tiwari <incredibleitsme404@gmail.com>
2025-01-22 13:19:34 +0545

    .

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -2,12 +2,12 @@ const express=require("express");
 const userRegister = require("./controllers/userRegister");
 const userLogin = require("./controllers/userLogin");
 const auth = require("../../middlewares/auth");
-const adminDashboard = require("./controllers/adminDashboard");
+
 const userRouter= express.Router();
 userRouter.post("/register",userRegister);
 userRouter.post("/login",userLogin);
 //Protected routes
-userRouter.use(auth);
-userRouter.get("/dashboard",adminDashboard);
+//userRouter.use(auth);
+//userRouter.get("/dashboard",adminDashboard);
 
 module.exports=userRouter;
