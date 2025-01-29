ef50e02cc63ddd754d7a7c404bef6a34ef3e9b6e
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2025-01-29 10:09:41 +0545

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const mongoose = require("mongoose");
 const userRouter = require("./modules/users/users.routes");
 const eventRouter = require("./modules/events/events.routes");
 const adminRouter = require("./modules/admin/admin.routes");
+
+//Utility routes
+const recommendationRouter = require("./utilities/utilities.routes");
+const { scheduleReminders } = require('./utilities/notificationService');
+
 require("dotenv").config();
 
 mongoose.connect(process.env.mongo_connect, {}).then(() => {
@@ -16,6 +21,11 @@ mongoose.connect(process.env.mongo_connect, {}).then(() => {
 app.use("/users", userRouter);
 app.use("/events", eventRouter);
 app.use("/admin", adminRouter);
+app.use("/api",recommendationRouter);
+
+
+// Start reminder service
+scheduleReminders();
 
 app.listen(8000, () => {
     console.log("Server started successfully!");
