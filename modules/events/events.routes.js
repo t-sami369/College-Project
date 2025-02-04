b1371e7d02436e407e107dfc0d7bd640aa2dabcc
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2025-02-04 20:21:59 +0545

    Update events.routes.js
    Routes for rating events.

--- modules/events/events.routes.js
+++ modules/events/events.routes.js
@@ -5,6 +5,7 @@ const deleteEvent = require("./controllers/deleteEvent");
 const updateEvent = require("./controllers/updateEvent");
 const getEvent = require("./controllers/getEvents");
 const registerEvent = require("./controllers/registerEvent");
+const rateEvent = require("./controllers/rateEvent");
 
 
 const eventRouter = express.Router();
@@ -17,5 +18,6 @@ eventRouter.post("/create", roleAuth(['organizer','admin']), createEvent);
 eventRouter.delete("/delete", roleAuth(['organizer','admin']), deleteEvent);
 eventRouter.patch("/update", roleAuth(['organizer','admin']), updateEvent);
 eventRouter.post("/register", auth, registerEvent);
+eventRouter.post("/rate", auth, rateEvent);
 
 module.exports=eventRouter;
