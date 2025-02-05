c1a686808828afb22703156a2fe9a46fee8d9eb8
Samiksha Tiwari <incredibleitsme404@gmail.com>
2025-02-05 13:01:43 +0545

    updating event schema

--- models/events.model.js
+++ models/events.model.js
@@ -1,44 +1,50 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const eventSchema= new mongoose.Schema({
+const eventSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: [true, "Event title is required!"],
-      },
-      description: {
-        type: String,
-        required: [true, "Event description is required!"],
-      },
-      date: {
-        type: Date,
-        required: [true, "Event date is required!"],
-      },
-      organizer: {
+      type: String,
+      required: [true, "Event title is required!"],
+    },
+    description: {
+      type: String,
+      required: [true, "Event description is required!"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Event date is required!"],
+    },
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users", // Reference to the user (organizer)
+      required: true,
+    },
+    // Adding status field to eventSchema
+    status: {
+      type: String,
+      enum: ["pending", "active", "completed"],
+      default: "pending",
+    },
+    image: {
+      type: String, // URL of the image
+      required: false,
+    },
+    attendees: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "users", // Reference to the user (organizer)
-        required: true,
+        ref: "users", // References registered users
       },
-      // Adding status field to eventSchema
-      status: {
-        type: String,
-        enum: ['pending', 'active', 'completed'],
-        default: 'pending'
+    ],
+    materials: [
+      {
+        name: String,
+        url: String,
       },
-      attendees: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "users", // References registered users
-        },
-      ],
-      materials: [
-        {
-          name: String,
-          url: String,
-        },
-      ],
-    }, {
-      timestamps: true
-    }
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Event = mongoose.model("events", eventSchema);
