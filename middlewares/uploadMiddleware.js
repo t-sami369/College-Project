dabc3b5c9287535fc85530fae0f9dea6c4c710a0
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2025-02-05 23:22:38 +0545

    Create uploadMiddleware.js
    Pic upload middleware

---
+++ middlewares/uploadMiddleware.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/events')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = /jpeg|jpg|png/;
+        const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = allowedTypes.test(file.mimetype);
+
+        if (extname && mimetype) {
+            return cb(null, true);
+        }
+        cb(new Error('Only .png, .jpg and .jpeg files are allowed'));
+    }
+});
+
+module.exports = upload;
