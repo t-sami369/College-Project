79624f419faf9b1e3b4935f63b9678580c5ee78f
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2025-02-10 18:26:59 +0545

    Update users.routes.js

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,16 +1,12 @@
 const express=require("express");
 const userRegister = require("./controllers/userRegister");
 const userLogin = require("./controllers/userLogin");
-const auth = require("../../middlewares/auth");
+const userDelete = require("./controllers/deleteUser");
+const {auth, roleAuth} = require("../../middlewares/auth");
 
 const userRouter= express.Router();
 userRouter.post("/register",userRegister);
 userRouter.post("/login",userLogin);
-//Protected routes
-// userRouter.use(auth);
-// router.post("/create", roleAuth(['organizer']), createEvent);
-// router.put("/update/:id", roleAuth(['organizer']), updateEvent);
-// router.delete("/delete/:id", roleAuth(['organizer']), deleteEvent);
-//userRouter.get("/dashboard",adminDashboard);
+userRouter.delete("/delete",auth, roleAuth("admin"), userDelete);
 
 module.exports=userRouter;
