700dbd4505258a94b8ffc899a82121a35d956287
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2025-02-10 18:26:49 +0545

    Update events.routes.js

--- modules/events/events.routes.js
+++ modules/events/events.routes.js
@@ -1,23 +1,24 @@
-const express=require("express");
+const express = require("express");
 const createEvent = require("./controllers/createEvent");
-const {auth,roleAuth} = require("../../middlewares/auth");
+const { auth, roleAuth } = require("../../middlewares/auth");
 const deleteEvent = require("./controllers/deleteEvent");
 const updateEvent = require("./controllers/updateEvent");
-const getEvent = require("./controllers/getEvents");
+const { getEventById, getEvents } = require("./controllers/getEvents");
 const registerEvent = require("./controllers/registerEvent");
 const rateEvent = require("./controllers/rateEvent");
 const upload = require('../../middlewares/uploadMiddleware');
 
-const eventRouter = express.Router();
-eventRouter.get("/",getEvent);
 
+const eventRouter = express.Router();
+eventRouter.get("/", getEvents);
+eventRouter.get("/:id", getEventById);
 
 //protected routes for event management
 eventRouter.use(auth);
 eventRouter.post("/create", roleAuth(['organizer','admin']), upload.single('image'), createEvent);
-eventRouter.delete("/delete", roleAuth(['organizer','admin']), deleteEvent);
+eventRouter.delete("/delete", roleAuth(["organizer", "admin"]), deleteEvent);
 eventRouter.patch("/update", roleAuth(['organizer','admin']), upload.single('image'), updateEvent);
 eventRouter.post("/register", auth, registerEvent);
 eventRouter.post("/rate", auth, rateEvent);
 
-module.exports=eventRouter;
+module.exports = eventRouter;
