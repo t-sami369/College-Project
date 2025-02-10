daf821af1e2dfa5f32dfa9bc7cc8dc438c222865
Pratik Shah <111047921+Lucifer98712@users.noreply.github.com>
2025-02-10 18:26:34 +0545

    Update createEvent.js

--- modules/events/controllers/createEvent.js
+++ modules/events/controllers/createEvent.js
@@ -1,46 +1,47 @@
-const Event=require("../../../models/events.model");
+const Event = require("../../../models/events.model");
 const Notification = require("../../../models/notification.model");
 const User = require("../../../models/users.model");
-const { sendNewEventNotification } = require('../../../utilities/notificationService');
+const {
+  sendNewEventNotification,
+} = require("../../../utilities/notificationService");
 
-const createEvent =async(req,res)=>{
-  
-  const {title, description,date}=req.body;
-  try{
-    const newEvent= await Event.create({
+const createEvent = async (req, res) => {
+  const { title, description, date } = req.body;
+  try {
+    const newEvent = await Event.create({
       title,
       description,
       date,
-      organizer:req.user.id,
+      organizer: req.user.id,
       image: req.file ? `/uploads/events/${req.file.filename}` : null
-    });
+      });
 
     // Create notifications for all users
     const users = await User.find({});
-    await Promise.all(users.map(user => 
-      Notification.create({
-        userId: user._id,
-        eventId: newEvent._id,
-        message: `New event created: ${newEvent.title}`
-      })
-    ));
+    await Promise.all(
+      users.map((user) =>
+        Notification.create({
+          userId: user._id,
+          eventId: newEvent._id,
+          message: `New event created: ${newEvent.title}`,
+        })
+      )
+    );
 
     // Send email notifications
     await sendNewEventNotification(newEvent);
-  
+
     res.status(200).json({
-      status:"success",
-      message:"Event created successfully!",
-      event:newEvent
+      status: "success",
+      message: "Event created successfully!",
+      event: newEvent,
     });
-  }catch(e){
+  } catch (e) {
     res.status(400).json({
-      status:"failed",
-      error:e.message
+      status: "failed",
+      error: e.message,
     });
     return;
   }
-  
-   
 };
-module.exports=createEvent;
+module.exports = createEvent;
